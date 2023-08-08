ce96e5f47bf7529f139944beecfe4f5de631e04c
manishareddy3012 <manishareddy3012@gmail.com>
2023-08-09 00:18:04 +0530

    first commit

---
+++ calci.py
@@ -0,0 +1,15 @@
+a=int(input("enter number"))
+b=int(input("enter number"))
+d=input("enter operator")
+if(d=="+"):
+    print("sum is",a+b)
+elif(d=="-"):
+    print("Sub is",a-b)
+elif(d=="*"):
+    print("mul is",a*b)
+elif(d=="/"):
+    print("div is",a/b)
+elif(d=="//"):
+    print("quot is",a//b)
+elif(d=="%"):
+    print("rem is",a%b)
